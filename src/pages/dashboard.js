a4afb9d2a255ad52dd5914b3a1ad88d52abb2269
Stephen Walsh <stp.walsh33@gmail.com>
2021-12-21 03:48:41 -0500

    Updated dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,16 +1,84 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {withSnackbar} from "notistack";
-import { Button, Card, CardContent, Container, FormControl, TextField } from '@material-ui/core';
+import { Card, CardContent, Container } from '@material-ui/core';
+import { Redirect } from 'react-router-dom';
+import ReactTable from 'react-table';
+import { getData } from '../api'
 
 class Dashboard extends Component {
   state = {
+    data: [],
+    pageSize: 10,
+    page: 0
+  }
+
+  componentDidMount() {
+    let data = getData();
+    this.setState({ data });
   }
 
   render() {
+		if (!this.props.isLoggedIn) {
+      return (
+        <Redirect
+          to={{
+            pathname: '/',
+          }} />
+      )
+    }
+
+    const { data, pageSize, page } = this.state;
+
     return (
-      <Container maxWidth="sm" className="dashboard-page">
-        Dashboard
+      <Container maxWidth="lg" className="dashboard-page">
+        <Card>
+          <CardContent>
+            <h1>My AWS Instances</h1>
+            <ReactTable
+              data={data}
+              className="-striped -highlight"
+              defaultPageSize={10}
+              pageSize={pageSize}
+              page={page}
+              onPageChange={e => this.setState({ page: e })}
+              onPageSizeChange={e => this.setState({ pageSize: e })}
+              minRows={10}
+              columns={[
+                {
+                  Header: 'ID',
+                  id: 'id',
+                  accessor: 'id'
+                },
+                {
+                  Header: 'Type',
+                  id: 'type',
+                  accessor: 'type'
+                },
+                {
+                  Header: 'State',
+                  id: 'state',
+                  accessor: 'state'
+                },
+                {
+                  Header: 'AZ',
+                  id: 'az',
+                  accessor: 'az'
+                },
+                {
+                  Header: 'Public IP',
+                  id: 'public_ip',
+                  accessor: 'public_ip'
+                },
+                {
+                  Header: 'Private IP',
+                  id: 'private_ips',
+                  accessor: 'private_ips'
+                },
+              ]}
+            />
+          </CardContent>
+        </Card>
       </Container>
     );
   }
@@ -18,6 +86,7 @@ class Dashboard extends Component {
 
 function mapStateToProps(state) {
   return {
+    isLoggedIn: state.auth.isLoggedIn
   }
 }
 
--- src/redux/store.js
+++ src/redux/store.js
@@ -7,7 +7,7 @@ import rootReducer from './reducers';
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['auth']
+  blacklist: ['']
 }
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
